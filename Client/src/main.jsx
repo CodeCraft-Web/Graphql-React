757bd6a40561b9f353ec42605bb46e61f3b81336
Harsha Buddhika <59781888+Harsha-Buddhika-Official@users.noreply.github.com>
2025-10-04 09:21:16 +0530

    Update main.jsx

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -2,15 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
-import { ApolloProvider } from "@apollo/client/react";
-
-const client = new ApolloClient({
-  link: new HttpLink({
-    uri: "http://localhost:4000/graphql",
-  }),
-  cache: new InMemoryCache(),
-});
+import { client, ApolloProvider } from "./apollo.js";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
